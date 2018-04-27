bfeadb050f2bc8b802948b66a6ef92bc095b3699
liaojing1 <1964197177@qq.com>
2018-04-27 09:36:59 +0800

    修改文件

--- java01/src/java01/HelloWorld.java
+++ java01/src/java01/HelloWorld.java
@@ -3,6 +3,7 @@ package java01;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World of Java");
+		System.out.println("Hello World of Java");
 	}
 
 }
